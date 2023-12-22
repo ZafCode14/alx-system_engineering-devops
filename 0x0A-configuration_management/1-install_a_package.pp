d96616d624752a3ff6cded53b7db2b596edd6522
ZafCode14 <zafarwp14@gmail.com>
2023-12-22 20:15:04 +0200

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 # Installing flask from pip3 version 2.1.0
-package { 'python3-pip':
-  ensure => installed,
-}
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
