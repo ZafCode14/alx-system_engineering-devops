f75d17c709b8f23bdf1e511bccca34f004b188ae
ZafCode14 <zafarwp14@gmail.com>
2023-12-23 06:21:24 +0200

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -13,3 +13,9 @@ package { 'flask':
   provider => 'pip3',
   require  => Package['python3-pip']
 }
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip']
+}
