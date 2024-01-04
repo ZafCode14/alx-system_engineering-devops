58ea6cd703f18515437fcb3cdeb887c421a93f2d
ZafCode14 <zafarwp14@gmail.com>
2024-01-04 08:26:12 +0200

    7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,66 +1,24 @@
 # configure nginx
+
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
+
 package { 'nginx':
-  ensure => installed,
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-# Configure Nginx server
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => '
-    server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        root /var/www/html;
-        index index.html index.htm index.nginx-debian.html;
-
-        location / {
-            return 200 "Hello World!";
-        }
-
-        location /redirect_me {
-            return 301 http://example.com/redirected_page;
-        }
-    }
-  ',
-  notify  => Service['nginx'],
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-# Set up custom 404 error page
-file { '/var/www/html/404.html':
-  ensure  => file,
-  content => '
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Error 404 - Not Found</title>
-    </head>
-    <body>
-        <h1>Error 404 - Not Found</h1>
-        <p>Ceci n\'est pas une page.</p>
-    </body>
-    </html>
-  ',
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-# Configure Nginx to use custom 404 error page
-file { '/etc/nginx/nginx.conf':
-  ensure  => present,
-  content => "
-    include /etc/nginx/mime.types;
-    default_type application/octet-stream;
-
-    error_page 404 /404.html;
-
-    # rest of nginx.conf configuration...
-  ",
-  notify  => Service['nginx'],
-}
-
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  hasstatus => true,
-  restart   => '/bin/systemctl reload nginx',
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
