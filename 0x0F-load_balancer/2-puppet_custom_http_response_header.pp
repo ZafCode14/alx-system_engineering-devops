fc7f24ddcb3ec4df4b7e237bf4b9727876f9779d
ZafCode14 <zafarwp14@gmail.com>
2024-01-08 17:10:19 +0200

    2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,25 @@
 # creating a custum HTTP header response with Puppet
-
-exec { 'update_system':
+#
+exec { 'update system':
         command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
 	ensure => 'installed',
-	require => Exec['update_system']
+	require => Exec['update system']
 }
 
 file {'/var/www/html/index.html':
 	content => 'Hello World!'
 }
 
-exec {'add_header':
-	command => 'sed -i "24i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
 
