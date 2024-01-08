d7d7ea1e19e34284781d4c377b67dbe1a1bda24a
ZafCode14 <zafarwp14@gmail.com>
2024-01-08 17:03:45 +0200

    2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,28 @@
 # creating a custum HTTP header response with Puppet
+#
+exec { 'update_apt':
+  command   => 'apt-get update',
+  provider  => shell,
+  logoutput => true,
+}
 
-exec { 'command':
-  command  => 'apt-get update;
-  apt-get install nginx -y;
-  sudo sed -i "24i\\n\tadd_header X-Served-By $hostname;" /etc/nginx/sites-available/default;
-  sudo service nginx restart',
-  provider => shell,
+exec { 'install_nginx':
+  command   => 'apt-get install nginx -y',
+  provider  => shell,
+  require   => Exec['update_apt'],
+  logoutput => true,
+}
+
+exec { 'add_header':
+  command   => "sudo sed -i '24i\\n\tadd_header X-Served-By $hostname;' /etc/nginx/sites-available/default",
+  provider  => shell,
+  require   => Exec['install_nginx'],
+  logoutput => true,
+}
+
+exec { 'restart_nginx':
+  command   => 'service nginx restart',
+  provider  => shell,
+  require   => Exec['add_header'],
+  logoutput => true,
 }
